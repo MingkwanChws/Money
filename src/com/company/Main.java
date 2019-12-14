4973ae7750175cf50cc85b7338fa6d9b168a7b58
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-14 14:34:51 +0700

    Add while loop and print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
 
 public class Main {
-    // ฝากธนาคารด้วยเงินต้น 500 บาท ได้ดอกเบี้ยปีละ 1.5% กี่ปีถึงจะมีเงินในบัญชีทั้งหมดมากกว่า 1,0000 บาท
+    // ฝากธนาคารด้วยเงินต้น 500 บาท ได้ดอกเบี้ยปีละ 1.5% กี่ปีถึงจะมีเงินในบัญชีทั้งหมดมากกว่า 1,000 บาท
     public static void money() {
         float deposit = 500.0f;
         float interestRate = .015f;
-        int year = 1;
+        int years = 1;
         float balance = deposit * (1 + interestRate); // 500 * (1.015)
+        while (balance <= 1000) {
+            balance = balance * (1 + interestRate);
+            years++;
+        }
+        System.out.println(years);
+        System.out.println(balance);
     }
     public static void main(String[] args) {
+        money();
     }
 }
